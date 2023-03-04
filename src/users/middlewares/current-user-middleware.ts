222a7a681f5a89589699c865d67a4302a7f7b2ae
Rakshith N <rakshithacchu9625@gmail.com>
2023-03-04 10:05:48 +0530

    Updated middleware

--- src/users/middlewares/current-user-middleware.ts
+++ src/users/middlewares/current-user-middleware.ts
@@ -1,6 +1,7 @@
 import {  NestMiddleware } from "@nestjs/common";
 import { InjectRepository } from "@nestjs/typeorm";
 import { Request, Response, NextFunction } from 'express'
+import { Repository } from "typeorm";
 import { Users } from "../users.entity";
 import { UsersService } from "../users.service";
 
@@ -15,12 +16,12 @@ declare global {
 export class CurrentUserMiddleware implements NestMiddleware {
     constructor(
         @InjectRepository(Users) 
-        private userService:UsersService
+        private userRepository:Repository<Users>
 
     ) { }
     async use(req: Request, res: Response, next: NextFunction) {
         if (req.session.userId) {
-            const user = await this.userService.findOne(req.session.userId);
+            let user=await this.userRepository.findOne({ "where": { "id": req.session.userId } ,relations:['role']});
             req.currentUser = user;
         }
         next();
